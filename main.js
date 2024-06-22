8b9f2f10e1d443b8c0504cb28a2fca678bf1c66f
Pratik Chapagain <chapagainpratik28@gmail.com>
2024-06-22 21:25:11 +0545

    push

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-let score = prompt("Enter your score (0-100)");
-let grade;
-
-if(score>=90 && score<=100){
-    grade = "A";
-} 
-else if(score>=70 && score<=89){
-        grade = "B";
-} 
-else if(score>=60 && score<=69){
-    grade = "C";
+for(let count=1;count<=5;count++){
+    console.log("Pratik Chapagain")
 }
-else if(score>=50 && score<=59){
-    grade = "D"; 
-}
-else if(score>=0 && score<=49){
-    grade = "F";
-}
-
-console.log("Your grade is:", grade);
