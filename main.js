0715ca5007b5e0207ddae6d1b88f99ab09a6db6c
Pratik Chapagain <chapagainpratik28@gmail.com>
2024-06-22 21:55:44 +0545

    push

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 // for of loop
 
 let str= "Pratik Chapagain";
+
+let size=0;
  for (let i of str)
     {
         console.log("i+",i);
+        size++;
     }
+
+     console.log("string size=", size);
