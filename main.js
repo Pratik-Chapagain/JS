18910a0b4ee0a424a51416a979a811e0d19bab09
Pratik Chapagain <chapagainpratik28@gmail.com>
2024-06-23 21:58:37 +0545

    push

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-// strings 
-let str ="PratikChapagain"
-let str2 = 'ee';
+let obj ={
+    item : "pen",
+    price: 10,
+
+};
+let output= `the cost of ${obj.item} is ${obj.price} rupees`;
+console.log(output);
+
+
+// template literals
+let specialstring =`this is a simple string`;
+console.log(typeof specialstring);
