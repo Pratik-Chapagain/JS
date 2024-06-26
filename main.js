4faca5a9c4c7289d9e272e5f6d566c880a7e6e63
Pratik Chapagain <chapagainpratik28@gmail.com>
2024-06-26 10:08:03 +0545

    push

---
+++ main.js
@@ -0,0 +1,13 @@
+function one(){
+    let username = "pratik"
+
+    function two(){
+        let website = "torrent"
+        console.log(username);
+    }
+    //console.log(website);
+
+    two()
+}
+
+one()
