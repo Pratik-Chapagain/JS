1e04b0543bfcf3f2bdfe91b009f3692df169f18a
Pratik Chapagain <chapagainpratik28@gmail.com>
2024-06-26 10:26:44 +0545

    push

--- main.js
+++ main.js
@@ -1,43 +1,9 @@
-function one(){
-    let username = "pratik"
+let user = {
+    username: "pratik",
+    price : 800,
 
-    function two(){
-        let website = "torrent"
-        console.log(username);
+    welcome : function(){        
+        console.log(`${this.username} , welcome to website`);
     }
-    //console.log(website);
-
-    two()
+    
 }
-
-one()
-
-
-
-if (true){
-    let username = "pratik"
-    if (username === "pratik"){
-        let website = "torrent"
-        console.log(username + website);
-    }
-    //console.log(website);
-}
-
-//console.log(username);
-
-
-
-//++++++++++++ inetresting +++++++++++
-
-console.log(addone(5))
-
-function addone(num){
-    return num + 1
-
-}
-
-addtwo(5)
-let addtwo = function(num){
-    return num + 2
-}
-
