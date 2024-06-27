7dfa2e135e780d8b9a8877657bc517c80fc676f1
Pratik Chapagain <chapagainpratik28@gmail.com>
2024-06-27 17:31:38 +0545

    push

--- main.js
+++ main.js
@@ -1,32 +1,3 @@
-let user = {
-    username: "pratik",
-    price : 800,
-
-    welcome : function(){        
-        console.log(`${this.username} , welcome to website`);
-    }
-    
+function chai(){
+    console.log(`DB CONNECTED`);
 }
-
-
-//user.welcome()
-//user.username = " sam "
-//user.welcome()
-
-//console.log(this);
-
-let chai = () => {
-    let username = "pratik"
-    console.log(this);
-}
-
-//chai ()
-
- //let addtwo =(num1 , num2)=> num1 + num2 
- //let addtwo =(num1 , num2)=> (num1 + num2) 
- 
-
- let addtwo = (nu)
- console.log(addtwo(3, 4));
-
-    
